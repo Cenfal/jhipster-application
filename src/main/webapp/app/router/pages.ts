0d7dd6fb4929c22a94f05ceb9378be7b963d01a2
piacbahadir <Cemil.Bahadir@vodafone.com>
2021-02-21 04:05:45 +0300

    main page hello world

--- src/main/webapp/app/router/pages.ts
+++ src/main/webapp/app/router/pages.ts
@@ -3,8 +3,13 @@ import { Authority } from '@/shared/security/authority';
 // prettier-ignore
 
 // jhipster-needle-add-entity-to-router-import - JHipster will import entities to the router here
-
+const BlogMainPage = () => import('@/blog/main-page/main-page.vue')
 export default [
-  
   // jhipster-needle-add-entity-to-router - JHipster will add entities to the router here
-]
+  {
+    path: '/blog-main-page',
+    name: 'BlogMainPage',
+    component: BlogMainPage,
+    meta: { authorities: [Authority.USER] },
+  },
+];
